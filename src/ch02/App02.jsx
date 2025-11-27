5fbcf176b5497a3084cb7770be4760124ec7ee98
gyeonggumun <lion989072@gmail.com>
2025-11-27 12:49:07 +0900

    Button 입력 및 초기화

--- src/ch02/App02.jsx
+++ src/ch02/App02.jsx
@@ -3,18 +3,53 @@ import "./style.css";
 
 function App02() {
 
-    const [name, setName] = useState("기본 이름");
+    const [name, setName] = useState("기본 이름"); // input의 value 값이 변하면 동작
+    const [addName, setNameButton] = useState("");  // 확인 버튼이 눌러지면 동작
+    const [age, setAge] = useState();
+    const [addAge, setAgeButton] = useState();
+    const [address, setAddress] = useState("주소");
+    const [addAddress, setAddressButton] = useState();
 
     const data = {
         title: "리엑트 기초 시작",
         nameValue: "기본 이름",
         age: "0",
+        address: "주소",
     }
+
 
     const handleNameInputOnChange = (event) => {
         console.log(event.target.value);
         setName(event.target.value);
     }
+    const handleAgeInputOnChange = (e) => {
+        setAge(e.target.value);
+    }
+    const handleAddressInputOnChange = (e) => {
+        setAddress(e.target.value);
+    }
+
+
+    const handleButtonOnClick = () => {
+        setNameButton(name);
+        setAgeButton(age);
+        setAddressButton(address);
+        setName("");
+        setAge("");
+        setAddress("");
+    }
+
+    
+
+    const handleResetOnClick = () => {
+        setNameButton("");
+        setAgeButton("");
+        setAddressButton("");
+        setName("");
+        setAge("");
+        setAddress("");
+    }
+
 
     return <div className="main-box">
         <div className="title-box">
@@ -22,16 +57,18 @@ function App02() {
         </div>
         <div className="input-box">
             <input type="text" value={name} onChange={handleNameInputOnChange}/>
-            <input type="text" value={data.age}/>
+            <input type="text" value={age} onChange={handleAgeInputOnChange}/>
+            <input type="text" value={address} onChange={handleAddressInputOnChange}/>
         </div>
         <div className="button-box">
-            <button>확인</button>
-            <button>초기화</button>
+            <button onClick={handleButtonOnClick} >확인</button>
+            <button onClick={handleResetOnClick} >초기화</button>
         </div>
         <div className="display-box">
             <ul>
-                <li>이름:{name} </li>
-                <li>나이: </li>
+                <li>이름:{addName} </li>
+                <li>나이: {addAge} </li>
+                <li>주소: {addAddress} </li>
             </ul>
         </div>
     </div>
