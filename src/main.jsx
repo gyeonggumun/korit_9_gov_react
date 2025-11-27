6f4cafeeb485a1540525fa0beb176af2c873e8c3
gyeonggumun <lion989072@gmail.com>
2025-11-27 17:39:50 +0900

    props, 컴포넌트, 구조분해 복습

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import App01 from "./ch01/App01";
 import App02 from "./ch02/App02";
 import Counter from "./ch02/Counter";
 import App02_2 from "./ch02/App02_2";
+import App02_3 from "./ch02/App02_3";
 
 const appObj = {
   //컴포넌트 렌더링
@@ -10,8 +11,9 @@ const appObj = {
     ch02: <App02 />, 
     counter: <Counter/>,
     ch02_2: <App02_2 />,
+    ch02_3: <App02_3 />,
 }
 
 // render()안에 있는 항목을 렌더링시킴
-createRoot(document.getElementById('root')).render(appObj.ch02_2);
+createRoot(document.getElementById('root')).render(appObj.ch02_3);
 
