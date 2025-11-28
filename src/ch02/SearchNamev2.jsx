f2552f2b620b2fcf928b0b38a7f2dbebfc2afadd
gyeonggumun <lion989072@gmail.com>
2025-11-28 14:46:11 +0900

    forEach, filter, find

--- src/ch02/SearchNamev2.jsx
+++ src/ch02/SearchNamev2.jsx
@@ -1,7 +1,8 @@
 function SearchNamev2() {
 const [result, setResult] = useState("값 없음");
 const [inputValue, setInputValue] = useState("");
-    const names = [
+
+    const profiles = [
         {
         name: "김준일",
         age: 32,
@@ -19,12 +20,35 @@ const [inputValue, setInputValue] = useState("");
         setInputValue(e.target.value);
         setResult("값 없음");
 
-        for (let i = 0; i < names.length; i++) {
-            const profile = names[i];
+        for (let i = 0; i < profiles.length; i++) {
+            const profile = profiles[i];
             if(profile.name === e.target.value) {
                 setResult(profile.age);
                 break;
             }
+        }
+
+        //forEach
+        profiles.forEach((profile) => {
+            if (profile.name === e.target.value) {
+                setResult(profile.age);
+            }
+        })
+
+        // filter
+        const foundProfiles = profiles.filter((profile) => {
+            return profile.name === e.target.value;
+        });
+        if (!!foundProfiles.length) {
+            setResult(foundProfiles[0].age);
+        }
+
+        // find
+        const foundProfile = profiles.find((profile) => {
+            return profile.name === e.target.value;
+        })
+        if (!!foundProfile) {
+            setResult(foundProfile.age);
         }
 
     }
