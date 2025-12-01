f7ab2b9bf8b04d3df0cefff56c9d81d723a1eec8
gyeonggumun <lion989072@gmail.com>
2025-12-01 16:11:23 +0900

    promise, async, await

--- src/ch04/Axios01.jsx
+++ src/ch04/Axios01.jsx
@@ -8,17 +8,17 @@ function Axios01() {
 
     // then이 useEffect보다 먼저 실행
     // 그렇기 때문에 요청이 많아지면 useEffect에 넣어 순서를 정해주는 것이 좋음
-    getUsersApi();
+    // getUsersApi();
 
-    const getUsersApi = async () => {
-        console.log("콘솔!!");
-        if(refetch) {
-            const response = await axios.get("http://192.168.2.101:8080/users");
-            console.log(response.data);
-            setUsers(response.data);
-            setRefetch(false);
-        }
-    }
+    // const getUsersApi = async () => {
+    //     console.log("콘솔!!");
+    //     if(refetch) {
+    //         const response = await axios.get("http://192.168.2.101:8080/users");
+    //         console.log(response.data);
+    //         setUsers(response.data);
+    //         setRefetch(false);
+    //     }
+    // }
     
 
     // useEffect(() =>{
@@ -30,7 +30,16 @@ function Axios01() {
     //             setRefetch(false);
     //         });
     //     }
-    // },[]);
+    // },[refetch]);
+
+    useEffect(() =>{
+        if(refetch) {
+            axios.get("http://192.168.2.101:8080/users")
+            .then(a => {
+                console.log(a.data[0].username);
+            });
+        }
+    },[refetch]);
    
    
    
--- src/ch04/Promise01.jsx
+++ src/ch04/Promise01.jsx
@@ -1,3 +1,5 @@
+import Swal from "sweetalert2";
+
 function Promise01() {
 
     const axios = {
@@ -31,8 +33,48 @@ function Promise01() {
     
     })
 
+    const getRequest = async (url) => {
+        const r = await axios.get(url);
+        console.log(r);
+        console.log(r.status);
+        console.log(r.data);
+        return r;   // 여러개의 주소를 변수에 담으려고 하면 return값을 주어야 한다
+    }
 
-    return <></>
+    const requests = async () => {  // 여러개의 주소값 순서대로 호출
+        const r1 = await getRequest("http://192.168.2.101:8080/nums");
+        const r2 = await getRequest("http://192.168.2.101:8080/names");
+        const r3 = await getRequest("http://192.168.2.101:8080/products");
+    }
+
+    // useEffect에는 async를 걸어줄 수 없음 then으로 처리
+    // async를 사용하려면 함수를 정의 하여 사용해주어야함
+    useEffect(() => {  
+        const a = async () => {
+            await getRequest();
+        };
+        a();
+    }, []);
+
+    const handleOnclick = async () => {
+        // Swal.fire({
+        //     title: "알림내용",
+        //     showCancelButton: true,
+        //     input: "text",
+        // }).then((r) => {
+        //     console.log(r);
+        //     console.log("콘솔");
+        // })
+        // alert("알림 내용");
+
+        const rs = await Swal.fire({title: "타이틀", input: "text"});
+        console.log(rs);
+    }
+
+
+    return <>
+        <button onClick={handleOnclick}>클릭</button>
+    </>
 
 }
 
